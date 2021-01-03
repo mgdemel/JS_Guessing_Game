0032baef0dd2c97797d392ca85400d10d6172132
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-03 20:50:25 +0200

    is this that famous variable feature I hear JS is known for?

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-function guess() {
-	document.getElementById("prompt").innerHTML = document.getElementById("number").value;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+	max = Math.floor(max);
+	let number = Math.floor(Math.random() * (max - min + 1)) + min;
+	document.getElementById("output").innerHTML = number;
+}
+
+function getPrompt() {
+
+	document.getElementById("ran").innerHTML = document.getElementById("output").value;
+	document.getElementById("button").innerHTML = document.getElementById("number").value;
+	if (document.getElementById("number").value > document.getElementById("randomNumber").value) 
+	{
+		document.getElementById("prompt").innerHTML = "Nope, too high!";
+	}
+	else if (document.getElementById("number").value < document.getElementById("randomNumber").value) 
+	{
+		document.getElementById("prompt").innerHTML = "Nope, too low!";
+	}
+	else
+	{
+		document.getElementById("prompt").innerHTML = "Yep, that's it!";
+	}
 }
